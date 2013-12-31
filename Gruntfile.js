565b5f51037b9658f3ab2f6d26c3743453cbf46b
Andrea Cadeddu <andrea.cadeddu@northwestern.edu>
2013-12-31 07:26:18 -0600

    contrib express and watch and now autorelading like a boss

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,42 @@
 module.exports = function(grunt) {
-grunt.initConfig({
-    connect: {
-        server: {
-            options: {
-                port: 9000,
-                keepalive: true,
-                base: 'www'
+    path = require('path')
+    grunt.initConfig({
+        express: {
+            all: {
+                options: {
+                    port: 9000,
+                    keepalive: true,
+                    bases: [path.join(__dirname,'www')],
+                    livereload: true
+                }
+            }
+        },
+        open:{
+            all:{
+                path: 'http://localhost:<%= express.all.options.port%>'
+            }
+        },
+        watch: {
+            all: {
+                options: { livereload: true },
+                files: [
+                    'www/js/*.js',
+                    'www/templates/*.html',
+                    'www/css/*.css',
+                    'www/index.html'
+                ]
+                //tasks: [''],
             }
         }
-    },
-  watch: {
-    all: {
-      options: { livereload: true },
-      files: [
-          'www/*.js',
-          'www/*.html'
-      ]
-      //tasks: [''],
-    },
-  }
-});
-grunt.loadNpmTasks('grunt-contrib-watch')
-grunt.loadNpmTasks('grunt-contrib-connect');
+    });
+grunt.loadNpmTasks('grunt-contrib-watch');
+//grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-express');
+grunt.loadNpmTasks('grunt-open');
+    grunt.registerTask('server', [
+        'express',
+        'open',
+//        'express-keepalive'
+        'watch'
+    ]);
 };
